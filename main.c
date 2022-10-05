d789051b9a34a9877996dbb97e5c4572ed551786
Navalkishor <navalkishor768@gmail.com>
2022-10-05 18:21:19 +0530

    added function to read TC72 temperature, added LSB reading and adding it in total temperature

--- main.c
+++ main.c
@@ -16,76 +16,86 @@
 #define MOSI		5
 #define SCK			7
 #define SS			4
-#define DIGIT0		0X01
-#define DIGIT1		0X02
-#define DECODE		0X09
-#define DECODE_R	0XFF
 
-unsigned char str[20];
+unsigned char str[20],str2[10];
 
 void spi_init()
 {
 	DDRB = (1<<MOSI)|(1<<SCK)|(1<<SS);  // set pin direction output
-	SPCR = (1<<SPE)|(1<<MSTR)|(1<<SPR0)|(1<<CPHA);
-}
-
-void spi_starttx(unsigned char tc72_write_address, unsigned char tc72_control)
-{
-	cbi(PORTB,SS);
-	//PORTB &= ~(1<<SS);
-	
-	SPDR = tc72_write_address;			
-	while(!(SPSR & (1<<SPIF)));	// wait till transfer finish
-	
-	SPDR = tc72_control;			
-	while(!(SPSR & (1<<SPIF)));	// wait till transfer finish
-	
-	sbi(PORTB,SS);
-	//PORTB |= (1<<SS);
+	SPCR = (1<<SPE)|(1<<MSTR)|(1<<SPR0)|(1<<CPHA);  // clock polarity set to 1, presaclar 128
 }
 
 void spi_tx(unsigned char data)
 {
 	//cbi(PORTB,SS);
-	//PORTB &= ~(1<<SS);
 
-	SPDR = data;			
+	SPDR = data;			    // load data to transfer
 	while(!(SPSR & (1<<SPIF)));	// wait till transfer finish
 	
 	//sbi(PORTB,SS);
-	//PORTB |= (1<<SS);
 }
 
 char spi_read()
 {	
 	//cbi(PORTB,SS);
-	//PORTB &= ~(1<<SS);
 	//SPDR = tc72_read_address;
 	while(!(SPSR & (1<<SPIF)));	// wait till transfer finish
 	
 	//sbi(PORTB,SS);
-	//PORTB |= (1<<SS);
 	
-	 return SPDR;
+	 return SPDR;               // return received data
 }
 
 void tc72Init(void){
-	cbi(PORTB,SS);
-	/*Select Control Register*/
+	
+	cbi(PORTB,SS);             // chip enable, TC72 is CS pin is active high
+	
 	spi_tx(0x80);
 	/*Select Continuous temperature Conversion*/
 	spi_tx(0x04);
-	sbi(PORTB,SS);
+	sbi(PORTB,SS);				// chip enable, TC72 is CS pin is active high
 	_delay_ms(150);
+}
+
+float get_tc72_temperature()
+{	
+	unsigned char tc72_raw[1];
+	
+	float tc72_measured;
+	
+	cbi(PORTB,SS);						// chip enable, TC72 is CS pin is active high
+	
+	spi_tx(0x01);						// address of MSB
+	
+	spi_tx(0x00);						// dummy flush to read with SCK cycle
+	
+	sbi(PORTB,SS);						// chip enable, TC72 is CS pin is active high
+	
+	tc72_raw[0]= spi_read();           // reading received data from spi buffer to char variable
+	
+	_delay_ms(10);
+	
+	cbi(PORTB,SS);						// chip enable, TC72 is CS pin is active high
+	
+	spi_tx(0x02);						// address of MSB
+	
+	spi_tx(0x00);						// dummy flush to read with SCK cycle
+	
+	sbi(PORTB,SS);						// chip enable, TC72 is CS pin is active high
+	
+	tc72_raw[1]=spi_read();             // reading received data from spi buffer to char variable
+	
+	tc72_measured = tc72_raw[1] + (tc72_raw[0]>>6)*0.25 ;     // converting received 8 bit to int
+	
+	return tc72_measured;
 }
 
 int main(void)
 {
-	char count=0,tc72_raw[1];
-	short int tc72_temperature;
-	DDRA = 0xff;
+	float tc72_temperature;
 	
-	PORTA = 0x00;
+	DDRA = 0xff;
+	PORTB = 0x00;
 	
 	/* Replace with your application code */
 	spi_init();
@@ -96,40 +106,33 @@ int main(void)
 	
 	while (1)
 	{
-		cbi(PORTB,SS);
+		tc72_temperature = get_tc72_temperature();
 		
-		spi_tx(0x02);
-		
-		spi_tx(0x00);
-		
-		sbi(PORTB,SS);
-		
-		//_delay_ms(1);
-		
-		tc72_raw[1]=spi_read();
-		
-		tc72_temperature = tc72_raw[1]; 
-		
-		if(tc72_temperature >= 200)
+		if(tc72_temperature >= 200)			// condition to print negative temperature values
 		{
-			tc72_temperature = (256-tc72_temperature)*(-1);
+			tc72_temperature = (256.0-tc72_temperature)*(-1.0);
 			
-			sprintf(str,"temp is %d",tc72_temperature);
+			dtostrf(tc72_temperature,3,2,str2);
+			
+			sprintf(str,"Temperature is %s",str2);
 		}
 		
-		else ;
+		else 
+		{	/* if temperature is positive, same will be printed,
+				converting temperature to string format
+			*/
 		
-		sprintf(str,"temp is %d",tc72_temperature);
+			dtostrf(tc72_temperature,3,2,str2);
+			
+			sprintf(str,"Temperature is %s",str2);
+		}
 		
-		uart_txstr(str);
-		//uart_tx('8');
+		
+		uart_txstr(str);							
+
 		uart_tx('\r');
 		
-		//uart_tx(tc72_raw[1]);
-		
-		PORTA = tc72_raw[1];
-		_delay_ms(1500);
-		
+		_delay_ms(500);
 		
 	}
 }
