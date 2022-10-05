13e5e5e495d8b3a73c5a837aa416bb173ad3ce0b
Navalkishor <navalkishor768@gmail.com>
2022-10-05 10:43:15 +0530

    reading successful with CPHA set to 1

--- main.c
+++ main.c
@@ -21,80 +21,113 @@
 #define DECODE		0X09
 #define DECODE_R	0XFF
 
+unsigned char str[20];
+
 void spi_init()
 {
 	DDRB = (1<<MOSI)|(1<<SCK)|(1<<SS);  // set pin direction output
-	SPCR = (1<<SPE)|(1<<MSTR)|(1<<SPR0);
+	SPCR = (1<<SPE)|(1<<MSTR)|(1<<SPR0)|(1<<CPHA);
 }
 
-void max7221_tx(unsigned char max7221_cmd, unsigned char dec_num)
+void spi_starttx(unsigned char tc72_write_address, unsigned char tc72_control)
 {
-	//cbi(PORTB,SS);
-	PORTB &= ~(1<<SS);
+	cbi(PORTB,SS);
+	//PORTB &= ~(1<<SS);
 	
-	SPDR = max7221_cmd;			// decimal select for data write
+	SPDR = tc72_write_address;			
 	while(!(SPSR & (1<<SPIF)));	// wait till transfer finish
 	
-	SPDR = dec_num;			// write decimal data
+	SPDR = tc72_control;			
 	while(!(SPSR & (1<<SPIF)));	// wait till transfer finish
 	
-	//sbi(PORTB,SS);
-	PORTB |= (1<<SS);
+	sbi(PORTB,SS);
+	//PORTB |= (1<<SS);
 }
 
 void spi_tx(unsigned char data)
 {
 	//cbi(PORTB,SS);
-	PORTB &= ~(1<<SS);
+	//PORTB &= ~(1<<SS);
 
-	SPDR = data;			// write decimal data
+	SPDR = data;			
 	while(!(SPSR & (1<<SPIF)));	// wait till transfer finish
 	
 	//sbi(PORTB,SS);
-	PORTB |= (1<<SS);
+	//PORTB |= (1<<SS);
 }
 
-unsigned char spi_read(unsigned char data)
+char spi_read()
 {	
-	unsigned char rawread;
 	//cbi(PORTB,SS);
-	PORTB &= ~(1<<SS);
-	SPDR = data;
+	//PORTB &= ~(1<<SS);
+	//SPDR = tc72_read_address;
 	while(!(SPSR & (1<<SPIF)));	// wait till transfer finish
-	rawread = SPDR;
-	//sbi(PORTB,SS);
-	PORTB |= (1<<SS);
 	
-	 return rawread;
+	//sbi(PORTB,SS);
+	//PORTB |= (1<<SS);
+	
+	 return SPDR;
+}
+
+void tc72Init(void){
+	cbi(PORTB,SS);
+	/*Select Control Register*/
+	spi_tx(0x80);
+	/*Select Continuous temperature Conversion*/
+	spi_tx(0x04);
+	sbi(PORTB,SS);
+	_delay_ms(150);
 }
 
 int main(void)
 {
-	unsigned char count=0,tc72_raw[1];
+	char count=0,tc72_raw[1];
+	short int tc72_temperature;
+	DDRA = 0xff;
 	
+	PORTA = 0x00;
 	
 	/* Replace with your application code */
 	spi_init();
 	
-	uart_init();
+	tc72Init();
 	
-	spi_tx(0x00);
-	
-	spi_tx(0x80);
-	
-	spi_tx(0x02);
+	uart_init();	
 	
 	while (1)
 	{
-		//spi_tx(0x02);
+		cbi(PORTB,SS);
 		
-		tc72_raw[0]=spi_read(0x02);
+		spi_tx(0x02);
 		
-		uart_tx('8');
+		spi_tx(0x00);
+		
+		sbi(PORTB,SS);
+		
+		//_delay_ms(1);
+		
+		tc72_raw[1]=spi_read();
+		
+		tc72_temperature = tc72_raw[1]; 
+		
+		if(tc72_temperature >= 200)
+		{
+			tc72_temperature = (256-tc72_temperature)*(-1);
+			
+			sprintf(str,"temp is %d",tc72_temperature);
+		}
+		
+		else ;
+		
+		sprintf(str,"temp is %d",tc72_temperature);
+		
+		uart_txstr(str);
+		//uart_tx('8');
 		uart_tx('\r');
 		
-		uart_tx(tc72_raw[0]);
+		//uart_tx(tc72_raw[1]);
 		
+		PORTA = tc72_raw[1];
 		_delay_ms(1500);
 		
 		
